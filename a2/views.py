6baf3c70b27b839439aa2c3894dee3f7ed9fc7ff
quheng <quheng@qh.local>
2016-05-04 15:54:48 +0800

    update structure

--- a2/views.py
+++ a2/views.py
@@ -1,7 +1,15 @@
-from flask import render_template
 from a2 import app
+from flask import render_template
+from templates.model import user
 
 @app.route("/")
 @app.route('/index')
 def index():
     return render_template("index.html")
+
+@app.route("/login")
+def login():
+    user = user.user()
+    user.name = "tom"
+    user.password = "123456"
+    return render_template("login.html", user = user)
