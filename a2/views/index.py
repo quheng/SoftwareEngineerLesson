3f2b376cda6110bcd0657cc0d68762f149e2168d
quheng <quheng@qh.local>
2016-05-05 20:38:29 +0800

    change structure

--- a2/views/index.py
+++ a2/views/index.py
@@ -5,10 +5,10 @@
 from flask import render_template
 from models import user
 from manager import app
-from a2_models import a2_index
+from templates.a2_models import a2_index
 
 @app.route('/a2')
 def a2():
     temp_data = a2_index.data()
     temp_data.userlist = user.User.get_users()
-    return render_template("a2.html", data = temp_data)
+    return render_template("a2/a2.html", data = temp_data)
