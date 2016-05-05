f598a9e7151a0f4a04a05ff5ca4bde4458dbd672
quheng <quheng@qh.local>
2016-05-05 21:50:31 +0800

    change structure

--- manager.py
+++ manager.py
@@ -8,15 +8,6 @@ import jinja2
 app = Flask(__name__)
 app.config.from_object('config')
 
-
-# add template
-template_path = []
-template_path.append(sys.path[0] + "/a2/templates")
-# template_path.append(sys.path[0] + "/a5/templates")
-
-template_loader = jinja2.ChoiceLoader([app.jinja_loader, jinja2.FileSystemLoader(template_path), ])
-app.jinja_loader = template_loader
-
 # database
 from flask.ext.sqlalchemy import SQLAlchemy
 db = SQLAlchemy(app)
