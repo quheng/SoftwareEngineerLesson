4be72cb59b1edd3ca9d9ecf9d92cb34278c60111
tlsshh <598991258@qq.com>
2016-05-19 12:29:48 +0800

    Revert "today final2"
    This reverts commit 67681e83d4449131476de44d58cce2e44c8e45c9.

--- api/__init__.py
+++ api/__init__.py
@@ -14,7 +14,7 @@ api.add_resource(getuser.GetUserID, '/a2/api/UserID')
 api.add_resource(order.selectOrder, '/a2/api/selectOrder')
 api.add_resource(order.deleteOrder, '/a2/api/deleteOrder')
 api.add_resource(order.insertOrder, '/a2/api/insertOrder')
-api.add_resource(order.selectOrderByID, '/a2/api/selectOrderByID')
+api.add_resource(order.selectOrderByID, '/a2/api/selectOrderBy')
 
 # api.add_resource(getuser.GetUserNickname, '/api/nickname')
 # api.add_resource(getseller.GetSellerID, '/api/SellerID'	)
