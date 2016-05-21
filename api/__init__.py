a4afeedf0e7738a1173528ded1c9e2c9dbe6081f
quheng <quheng@qh.local>
2016-05-21 11:47:34 +0800

    add test api

--- api/__init__.py
+++ api/__init__.py
@@ -6,3 +6,29 @@ from flask import Flask, jsonify, request
 Swagger(app)
 import complaint
 import order
+
+@app.route("/a2/api/test", methods=['POST'])
+def GetComplaintList():
+    """
+    use to test connection
+    ---
+    tags:
+      - test
+    parameters:
+      - name: para
+        in: query
+        type: integer
+        description: parameters test
+    responses:
+      200:
+        description: test
+        schema:
+          id: res
+          properties:
+            result:
+              type: string
+              description: The test
+              default: 'test'
+    """
+    para = request.args.get('para')
+    return jsonify({"result": para})
