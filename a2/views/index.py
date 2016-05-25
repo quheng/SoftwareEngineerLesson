d89da875aba7ac0a6629f55066739dc7da9b9682
quheng <quheng@qh.local>
2016-05-25 21:35:14 +0800

    add session

--- a2/views/index.py
+++ a2/views/index.py
@@ -2,13 +2,14 @@
 # coding=utf8
 # Author: quheng
 
-from flask import render_template
+from flask import render_template, session
 from manager import app
 from templates.a2_models import a2_index
 
 
 @app.route('/orderdetails', methods=['GET'])
 def orderdetails():
+    print session['user']
     return render_template("a2/orderdetails.html", userID = 1)
 
 @app.route('/complaint')
