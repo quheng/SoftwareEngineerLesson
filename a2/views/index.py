5a82576bb6b6b9735554863617e0f86bb27fdee2
quheng <quheng@qh.local>
2016-05-25 21:08:21 +0800

    add userID

--- a2/views/index.py
+++ a2/views/index.py
@@ -9,12 +9,12 @@ from templates.a2_models import a2_index
 
 @app.route('/orderdetails', methods=['GET'])
 def orderdetails():
-    return render_template("a2/orderdetails.html")
+    return render_template("a2/orderdetails.html", userID)
 
 @app.route('/complaint')
 def complaint():
-    return render_template("a2/complaint.html")
+    return render_template("a2/complaint.html", userID)
 
 @app.route('/orderlist')
 def orderlist():
-    return render_template("a2/orderlist.html")
+    return render_template("a2/orderlist.html", userID)
