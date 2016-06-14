f8e021ea7e6f7fcbe564511a454ee0e0ce6313d4
imzyy <zyy@imzyy.com>
2016-06-15 05:24:25 +0800

    modify index.py back...

--- a2/views/index.py
+++ a2/views/index.py
@@ -10,16 +10,25 @@ from templates.a2_models import a2_index
 @app.route('/orderdetails', methods=['GET'])
 def orderdetails():
     ID = request.cookies.get('kitty')
-    return render_template("a2/orderdetails.html", userID = 123)
+    if ID:
+        return render_template("a2/orderdetails.html", userID = ID)
+    else:
+        return redirect("http://121.42.175.1/login")
 
 
 @app.route('/complaint')
 def complaint():
     ID = request.cookies.get('kitty')
-    return render_template("a2/complaint.html", userID = 123)
+    if ID:
+        return render_template("a2/complaint.html", userID = ID)
+    else:
+        return redirect("http://121.42.175.1/login")
 
 
 @app.route('/orderlist')
 def orderlist():
     ID = request.cookies.get('kitty')
-    return render_template("a2/orderlist.html", userID = 123)
+    if ID:
+        return render_template("a2/orderlist.html", userID = ID)
+    else:
+        return redirect("http://121.42.175.1/login")
